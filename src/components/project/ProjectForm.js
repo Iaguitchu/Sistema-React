ccfaf932c37671812782db7262ec0683bbfec487
Iago de Andrade <iago2005andrade@gmail.com>
2025-09-23 21:11:59 -0300

    fazendo fetch com api

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -2,14 +2,34 @@ import Input from '../form/Input.jsx'
 import Select from '../form/Select.jsx'
 import SubmitButton from '../form/SubmitButton.jsx'
 import styles from './ProjectForm.module.css'
+import {useEffect, useState} from 'react'
 
 
 function ProjectForm({btnText}){
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() =>{
+        fetch("http://localhost:5000/categories",{ 
+        method: "GET",
+        headers:{
+            'Content-Type':'application/json'
+        }
+        })
+        .then((resp) => resp.json())
+        .then((data)=>{
+            setCategories(data)
+        })
+        .catch((err) => console.log(err))
+    },[])
+    
+    
+
     return( 
     <form className = {styles.form} >
         <Input type = "text" text = "Nome do projeto" name = "name" placeholder="Insira o nome do projeto"/>
         <Input type = "number" text = "Orçamento do projeto" name = "budget" placeholder="Insira o orçamento total"/>
-        <Select name = "Category_id" text = "Selecione a categoria"/>
+        <Select name = "Category_id" text = "Selecione a categoria" options={categories}/>
         <SubmitButton text={btnText} />
     </form>
     )
